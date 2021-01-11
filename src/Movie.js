36125c5046078eb14c6842d43019bdaea0bcb2d4
Wooktti <jaewooki1234@gmail.com>
2021-01-11 22:52:40 +0900

    deploy

--- src/Movie.js
+++ src/Movie.js
@@ -10,12 +10,12 @@ function Movie({ year, title, summary, poster, genres}) {
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
+            <ul className="movie__genres">
                 {genres.map((genre, index) => (
                 <li key={index} className="genres__genre">{genre}</li>
                 ))}
             </ul>
-            <p className="movie__summary">{summary}</p>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
     </div>
     );
@@ -28,6 +28,6 @@ Movie.propTypes = {
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired
-}
+};
 
 export default Movie;
